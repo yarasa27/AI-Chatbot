ddebaefb7f23fce31591582d154dac9f549c60b4
yarasa27 <elcan.abbasoff27@gmail.com>
2025-06-02 16:26:42 +0400

    bot logo is loading /JS/CSS/

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const promptInput = promptForm.querySelector(".prompt-input");
 let userMessage = "";
 
 // Function to create message elements
-const createMsgElement = (content, className) => {
+const createMsgElement = (content, ...classes) => {
     const div = document.createElement("div");
-    div.classList.add("message", className);
+    div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
 }
@@ -17,6 +17,8 @@ const handleFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
     if(!userMessage) return;
+
+    promptInput.value = "";
 
     // Generate user message HTML and add in the chats container
     const userMsgHTML = `<p class="message-text"></p>`;
@@ -28,7 +30,7 @@ const handleFormSubmit = (e) => {
     setTimeout(() => {
         // Generate bot message HTML and add in the chats container in 600ms
         const botMsgHTML = `<img src="gemini.svg" class="avatar"><p class="message-text">Just a sec..</p>`;
-        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message");
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
     }, 600);
 }
