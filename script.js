d64c9422b1fc8fb8c415c782bdd916a1f987e270
yarasa27 <elcan.abbasoff27@gmail.com>
2025-06-02 16:53:33 +0400

    Setting up Free Gemini API

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
+// API Setup
+const API_KEY = "AIzaSyA2hhKbdbWKbFF-mtoVaIXwM7BGIJhLvpE"
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+
 let userMessage = "";
+const chatHistory = [];
 
 // Function to create message elements
 const createMsgElement = (content, ...classes) => {
@@ -10,6 +15,34 @@ const createMsgElement = (content, ...classes) => {
     div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
+}
+
+// Make the API call and generate the bot's response
+const generateReponse = async (botMsgDiv) => {
+    const textElement = botMsgDiv.querySelector(".message-text")
+
+    // Add user message to the chat history
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: userMessage }]
+    });
+
+    try{
+        // Send the chat history to the API to get a response
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify( {contents: chatHistory} )
+        });
+        const data = await response.json();
+        if(!response.ok) throw new Error(data.error.message);
+
+        // Process the response text and display it
+        const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
+        textElement.textContent = responseText;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Handle the form submission
@@ -28,10 +61,11 @@ const handleFormSubmit = (e) => {
     chatsContainer.appendChild(userMsgDiv);
 
     setTimeout(() => {
-        // Generate bot message HTML and add in the chats container in 600ms
+        // Generate bot message HTML and add in the chats container after 600ms
         const botMsgHTML = `<img src="gemini.svg" class="avatar"><p class="message-text">Just a sec..</p>`;
         const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
+        generateReponse(botMsgDiv);
     }, 600);
 }
 promptForm.addEventListener("submit", handleFormSubmit);
