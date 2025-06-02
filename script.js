167d270f16efa4a7029839ed28cdedf259251340
yarasa27 <elcan.abbasoff27@gmail.com>
2025-06-02 16:00:23 +0400

    working bot in JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
@@ -17,9 +18,18 @@ const handleFormSubmit = (e) => {
     userMessage = promptInput.value.trim();
     if(!userMessage) return;
 
+    // Generate user message HTML and add in the chats container
     const userMsgHTML = `<p class="message-text"></p>`;
     const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
-    
+
     userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
+
+    setTimeout(() => {
+        // Generate bot message HTML and add in the chats container in 600ms
+        const botMsgHTML = `<img src="gemini.svg" class="avatar"><p class="message-text">Just a sec..</p>`;
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message");
+        chatsContainer.appendChild(botMsgDiv);
+    }, 600);
 }
 promptForm.addEventListener("submit", handleFormSubmit);
