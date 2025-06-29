703ed54ddf816f6b95b9b588106cefd191cc0009
yarasa27 <elcan.abbasoff27@gmail.com>
2025-06-29 22:18:52 +0400

    AI Chatbot website finished

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ const typingEffect = (text, textElement, botMsgDiv) => {
     textElement.textContent = "";
     const words = text.split(" ");
     let wordIndex = 0;
-
     typingInterval = setInterval(() => {
         if(wordIndex < words.length) {
             textElement.textContent += (wordIndex === 0 ? "" : " ") + words[wordIndex++];
@@ -53,7 +52,6 @@ const generateReponse = async (botMsgDiv) => {
         role: "user",
         parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: (({ fileName, isImage, ...rest})=> rest)(userData.file) }] : [])]
     });
-
     try{
         // Send the chat history to the API to get a response
         const response = await fetch(API_URL, {
@@ -68,7 +66,6 @@ const generateReponse = async (botMsgDiv) => {
         // Process the response text and display with typing effect
         const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
         typingEffect(responseText, textElement, botMsgDiv);
-
         chatHistory.push({ role: "model", parts: [{ text: responseText }] });
     } catch (error) {
         textElement.style.color = "#d62939";    
@@ -85,21 +82,19 @@ const handleFormSubmit = (e) => {
     e.preventDefault();
     const userMessage = promptInput.value.trim();
     if(!userMessage || document.body.classList.contains("bot-responding")) return;
-
     promptInput.value = "";
     userData.message = userMessage;
-    document.body.classList.add("bot-responding");
+    document.body.classList.add("bot-responding", "chats-active");
     fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
 
     // Generate user message HTML with optional file attachment
     const userMsgHTML = `
-        <p class="message-text"></p>${userData.file.data ? (userData.file.isImage ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="img-attachment" />` : `<p class="file-attachment"><span class="material-symbols-rounded">description</span>${userData.file.fileName}</p>`) : ""}`;
-
+        <p class="message-text"></p>${userData.file.data ? (userData.file.isImage ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="img-attachment" />` : `<p class="file-attachment"><span class="material-symbols-rounded">description</span>${userData.file.fileName}</p>`) : ""}
+    `;
     const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
     userMsgDiv.querySelector(".message-text").textContent = userMessage;
     chatsContainer.appendChild(userMsgDiv);
     scrollToBottom();
-
     setTimeout(() => {
         // Generate bot message HTML and add in the chats container after 600ms
         const botMsgHTML = `<img src="gemini.svg" class="avatar"><p class="message-text">Just a sec..</p>`;
@@ -109,15 +104,12 @@ const handleFormSubmit = (e) => {
         generateReponse(botMsgDiv);
     }, 600);
 }
-
 fileInput.addEventListener("change", () =>{
     const file = fileInput.files[0];
     if(!file) return;
-
     const isImage = file.type.startsWith("image/");
     const reader = new FileReader();
     reader.readAsDataURL(file);
-
     reader.onload = (e) => {
         fileInput.value = "";
         const base64String = e.target.result.split(",")[1];
@@ -148,13 +140,28 @@ document.querySelector("#stop-response-btn").addEventListener("click", () => {
 document.querySelector("#delete-chats-btn").addEventListener("click", () => {
     chatHistory.length = 0;
     chatsContainer.innerHTML = "";
-    document.body.classList.remove("bot-responding");
+    document.body.classList.remove("bot-responding", "chats-active");
 });
 
+// Handle suggestions click
+document.querySelectorAll(".suggestions-item").forEach(item => {
+    item.addEventListener("click", () => {
+        promptInput.value = item.querySelector(".text").textContent;
+        promptForm.dispatchEvent(new Event("submit"));
+    });
+});
+
+// Toggle dark/light theme
 themeToggle.addEventListener("click", () => {
     const isLightTheme = document.body.classList.toggle("light-theme");
+    localStorage.setItem("themeColor", isLightTheme ? "light_mode" : "dark_mode");
     themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
 })
+
+// Set intial theme from local storage
+const isLightTheme = localStorage.getItem("themeColor") === "light_mode";
+document.body.classList.toggle("light-theme", isLightTheme);
+themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode";
 
 promptForm.addEventListener("submit", handleFormSubmit);
 promptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click());
